0a204e2eebc401c17725fec754c532563a970f3f
Matt Conrad <mmconrad@gmail.com>
2016-11-01 22:37:11 -0700

    Create Instructions scene/page

---
+++ Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class InstructionsController : MonoBehaviour {
+
+    void Update () {
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0)) {
+            Debug.Log("clicked screen");
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}
