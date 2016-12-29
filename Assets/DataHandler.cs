4f05c210584425cf498209c42ffe0d3737d00601
Matt Conrad <mmconrad@gmail.com>
2016-12-28 21:23:05 -0800

    Add config files and initialization of SDK for AWS

---
+++ Assets/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
