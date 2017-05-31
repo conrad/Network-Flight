572f8061dab839827f6adb13f26e216b90cd162c
Matt Conrad <mmconrad@gmail.com>
2017-05-31 11:50:52 +0200

    Add more assets involving shots

---
+++ Assets/Scripts/Objects/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+/**
+ * This class moves an object straight forward always, 
+ * as opposed to have the object follow a target, which happen in ObjectMover.
+ */ 
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+	Rigidbody rigidbody;
+
+
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
---
+++ Assets/Scripts/Objects/ShotDestroyer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotDestroyer : MonoBehaviour 
+{
+	public float lifetime;
+	public GameObject explosion;
+
+
+
+	void Start () 
+	{
+		Destroy(gameObject, lifetime);
+	}
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			// TODO: Trigger hit feedback for both players and reduce hit player's health.
+			Instantiate(explosion, other.transform.position, other.transform.rotation);
+		}
+
+		Destroy(gameObject);
+	}
+}
